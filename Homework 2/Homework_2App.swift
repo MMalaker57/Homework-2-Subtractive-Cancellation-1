5ea919499b1589bd6febb830fae29e6deda93ee7
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-01-28 11:18:35 -0600

    Initial Commit

---
+++ Homework 2/Homework_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_2App.swift
+//  Homework 2
+//
+//  Created by Matthew Malaker on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
