17bb995d87b709c219067c4fe6b63a454da89257
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-01-28 13:53:07 -0600

    Add precision fields and comments

--- Homework 2/2_1_2_Subtractive_Cancellation.swift
+++ Homework 2/2_1_2_Subtractive_Cancellation.swift
@@ -9,6 +9,14 @@ import Foundation
 import SwiftUI
 
 class Quadratic: NSObject, ObservableObject{
+    
+//This is a simple quadratic formula using the traditional formula:
+//             ________
+//          | /  2
+//  - b pm  |/ b  - 4ac
+//--------------------
+//          2a
+
     func method1(a: Double, b: Double, c: Double) -> (Double, Double){
     var root1 = 0.0
     var root2 = 0.0
@@ -16,6 +24,16 @@ class Quadratic: NSObject, ObservableObject{
     root2 = (-1*b-sqrt(pow(b,2)-(4*a*c)))/(2*a)
     return (root1,root2)
 }
+
+    
+//This function uses an alternative form of the quadratic formula"
+//          - 2c
+//  ----------------
+//          __
+//       | / 2
+//  b pm |/ b  - 4ac
+
+
 
 func method2(a: Double, b: Double, c: Double) -> (Double, Double){
     var root1 = 0.0
--- Homework 2/ContentView.swift
+++ Homework 2/ContentView.swift
@@ -20,11 +20,14 @@ struct ContentView: View {
     @State var methodTwoMinusString = ""
     @State var rootOneDifferenceString = ""
     @State var rootTwoDifferenceString = ""
+    @State var rootOnePrecisionString = ""
+    @State var rootTwoPrecisionString = ""
     @ObservedObject var quadraticFunction = Quadratic()
     var body: some View {
         
-        
+        //Stack of All Text Fields
         VStack{
+            //a input
             HStack{
                 Text("Input a")
                     .padding(.horizontal)
@@ -37,6 +40,7 @@ struct ContentView: View {
                     .padding(.top, 30)
                     .padding(.bottom, 0)
             }
+            //b input
             HStack{
                 Text("Input b")
                     .padding(.horizontal)
@@ -49,6 +53,7 @@ struct ContentView: View {
                     .padding(.top, 30)
                     .padding(.bottom, 0)
             }
+            //c input
             HStack{
                 Text("Input c")
                     .padding(.horizontal)
@@ -66,8 +71,9 @@ struct ContentView: View {
             Button("Calculate", action: {self.calculateAll(a: a, b: b, c: c)})
             //Clear Button
             Button("Clear", action: {self.clear()})
-            
+            //Group used to condense text fields to save space in VStack
             Group{
+                //Text for method one root one
                 HStack{
                     Text("Method One Root One")
                         .padding(.horizontal)
@@ -80,6 +86,7 @@ struct ContentView: View {
                         .padding(.top, 5)
                         .padding(.bottom, 0)
                 }
+                //Text for method one root two
                 HStack{
                     Text("Method One Root Two")
                         .padding(.horizontal)
@@ -92,6 +99,7 @@ struct ContentView: View {
                         .padding(.top, 5)
                         .padding(.bottom, 0)
                 }
+                //Text for method two root one
                 HStack{
                     Text("Method Two Root One")
                         .padding(.horizontal)
@@ -104,6 +112,7 @@ struct ContentView: View {
                         .padding(.top, 5)
                         .padding(.bottom, 0)
                 }
+                //Text for method two root two
                 HStack{
                     Text("Method Two Root Two")
                         .padding(.horizontal)
@@ -116,7 +125,8 @@ struct ContentView: View {
                         .padding(.top, 5)
                         .padding(.bottom, 0)
                 }
-                HStack{
+            //Text for the difference between methods for root 1 (Plus)
+            HStack{
                     Text("Root One Difference")
                         .padding(.horizontal)
                         .frame(width: 150)
@@ -128,9 +138,8 @@ struct ContentView: View {
                         .padding(.top, 5)
                         .padding(.bottom, 0)
                 }
-            
-            
             }
+            //Text for the difference between methods for root 2 (Minus)
             HStack{
                 Text("Root Two Difference")
                     .padding(.horizontal)
@@ -143,18 +152,67 @@ struct ContentView: View {
                     .padding(.top, 5)
                     .padding(.bottom, 0)
             }
+            //Text for which method is more precise for root one
+            HStack{
+                Text("Root One Precision")
+                    .padding(.horizontal)
+                    .frame(width: 150)
+                    .padding(.top, 5)
+                    .padding(.bottom, 0)
+                TextField("",text: $rootOnePrecisionString)
+                    .padding(.horizontal)
+                    .frame(width: 200)
+                    .padding(.top, 5)
+                    .padding(.bottom, 0)
+            }
+            //Text for which method is more precise for root two
+            HStack{
+                Text("Root Two Precision")
+                    .padding(.horizontal)
+                    .frame(width: 150)
+                    .padding(.top, 5)
+                    .padding(.bottom, 0)
+                TextField("",text: $rootTwoPrecisionString)
+                    .padding(.horizontal)
+                    .frame(width: 200)
+                    .padding(.top, 5)
+                    .padding(.bottom, 0)
+            }
             
         }
     }
+    
+//Function for calculating all relevant roots and precisions and converting them to strings
     func calculateAll(a:Double,b: Double, c: Double){
         var rootOne = 0.0
         var rootTwo = 0.0
         var rootOneM2 = 0.0
         var rootTwoM2 = 0.0
+        var root1M1Difference = 0.0
+        var root2M1Difference = 0.0
+        var root1M2Difference = 0.0
+        var root2M2Difference = 0.0
+        
         rootOne = quadraticFunction.method1(a: a, b: b, c:   c).0
         rootTwo = quadraticFunction.method1(a: a, b: b, c:   c).1
         rootOneM2 = quadraticFunction.method2(a: a, b: b, c:   c).0
         rootTwoM2 = quadraticFunction.method2(a: a, b: b, c:   c).1
+        root1M1Difference = rootOne - quadraticFunction.method1(a: a, b: b+0.1, c: c).0
+        root2M1Difference = rootTwo - quadraticFunction.method1(a: a, b: b+0.1, c: c).1
+        root1M2Difference = rootOneM2 - quadraticFunction.method1(a: a, b: b+0.1, c: c).0
+        root2M2Difference = rootTwoM2 - quadraticFunction.method1(a: a, b: b+0.1, c: c).1
+        if root1M1Difference>root1M2Difference{
+            rootOnePrecisionString = "Method 2 is More Precise"
+        }
+        if root1M1Difference<root1M2Difference{
+            rootOnePrecisionString = "Method 1 is More Precise"
+        }
+        if root2M1Difference>root2M2Difference{
+            rootTwoPrecisionString = "Method 2 is More Precise"
+        }
+        if root2M1Difference<root2M2Difference{
+            rootTwoPrecisionString = "Method 1 is More Precise"
+        }
         methodOnePlusString = String(rootOne)
         methodOneMinusString = String(rootTwo)
         methodTwoPlusString = String(rootOneM2)
@@ -162,6 +220,7 @@ struct ContentView: View {
         rootOneDifferenceString = String(rootOne-rootOneM2)
         rootTwoDifferenceString = String(rootTwo-rootTwoM2)
     }
+//Obligatory Clear function. Sets all paramters to initial values
     func clear(){
         aString = "0"
         bString = "0"
@@ -175,6 +234,8 @@ struct ContentView: View {
         methodTwoMinusString = ""
         rootOneDifferenceString = ""
         rootTwoDifferenceString = ""
+        rootTwoPrecisionString = ""
+        rootOnePrecisionString = ""
     }
     
 }
